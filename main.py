a4b9ed3a5857338c163115e48eaf13f6d08f5425
niltonsainz <161954152+niltonsainz@users.noreply.github.com>
2024-05-10 21:51:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main():
     print('este é um script python criado por Henrique Ortiz')
     print('Alceu editou')
+    print('Nilton Sainz editou o script')
 if __name__ == '__main__':
     main()
